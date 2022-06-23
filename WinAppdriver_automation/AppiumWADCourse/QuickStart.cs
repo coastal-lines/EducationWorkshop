ff1795818797b02f0c4aa0c55249621cde6f2c9e
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-23 11:44:35 +0200

    WAD course

---
+++ WinAppdriver_automation/AppiumWADCourse/QuickStart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+
+namespace AppiumWADCourse
+{
+    public class QuickStart
+    {
+        public void OpenNotepad()
+        {
+            WindowsDriver<WindowsElement> notePadSession;
+        }
+    }
+}
