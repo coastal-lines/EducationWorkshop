8075b2b2769b8ee06788df4c34aeba16162d86e9
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-23 15:38:07 +0200

    WAD course

--- WinAppdriver_automation/MSTestOverview/AlarmsAndClockSmokeTest.cs
+++ WinAppdriver_automation/MSTestOverview/AlarmsAndClockSmokeTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
 
 namespace MSTestOverview
 {
@@ -9,7 +11,14 @@ namespace MSTestOverview
         [TestMethod]
         public void TestAlarmsAndClockIsLaunchingSuccesssfully()
         {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            WindowsDriver<WindowsElement> session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+            session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //global waiting
 
+            Assert.AreEqual("Alarms & Clock", session.Title.ToString(), "Wrong title of app");
+
+            session.Quit();
         }
     }
 }
