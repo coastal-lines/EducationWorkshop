45f6570bca736a3cf45c122d0569688397c32063
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-24 14:24:51 +0200

    WAD course

--- WinAppdriver_automation/MSTestOverview/AlarmsAndClockSmokeTest.cs
+++ WinAppdriver_automation/MSTestOverview/AlarmsAndClockSmokeTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
 
 namespace MSTestOverview
 {
@@ -18,7 +20,7 @@ namespace MSTestOverview
             AppiumOptions appiumOptions = new AppiumOptions();
             appiumOptions.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
             session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
-            session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //global waiting
+            //session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //global waiting
         }
 
         [ClassCleanup]
@@ -43,6 +45,18 @@ namespace MSTestOverview
         [TestMethod]
         public void TestAlarmsAndClockIsLaunchingSuccesssfully()
         {
+            Assert.AreEqual("Alarms & Clock", session.Title.ToString(), $"Wrong title of WindowsAlarms: should be {"Alarms & Clock"} but was {session.Title.ToString()}");
+        }
+
+        [TestMethod]
+        public void WaitElementTest()
+        {
+            //var txtLocation = session.FindElementByName("Enter a location");
+
+            WebDriverWait wait = new WebDriverWait(session, TimeSpan.FromSeconds(10));
+            //Until - is a lambda method
+            wait.Until(pred => session.FindElementByName("Enter a location"));
+
             Assert.AreEqual("Alarms & Clock", session.Title.ToString(), $"Wrong title of WindowsAlarms: should be {"Alarms & Clock"} but was {session.Title.ToString()}");
         }
     }
---
+++ WinAppdriver_automation/MSTestOverview/DataDrivenTestingTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace MSTestOverview
+{
+    [TestClass]
+    class DataDrivenTestingTests
+    {
+        static WindowsDriver<WindowsElement> session;
+        private static TestContext objTestContext;
+
+        [ClassInitialize]
+        public static void PrepareForTestingAlarms(TestContext testContext)
+        {
+            Debug.WriteLine("ClassInitialize -> PrepareForTestingAlarms");
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+
+            objTestContext = testContext;
+        }
+
+
+    }
+}
