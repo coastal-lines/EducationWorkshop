e15427763dc04f52e9b6f2569602a85342db35aa
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-26 11:09:01 +0200

    WAD course

---
+++ WinAppdriver_automation/WinFormsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinFormsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinFormsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5534ff83-38ad-48a8-b22a-2a9f723b54c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WinAppdriver_automation/WinFormsTests/WinFormsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace WinFormsTests
+{
+    [TestClass]
+    public class WinFormsTests
+    {
+        static WindowsDriver<WindowsElement> sessionWinForm;
+
+        [ClassInitialize]
+        public static void SetupDriver()
+        {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            //appiumOptions.AddAdditionalCapability("app", @"C:\Work2\Projects\GIT\EducationWorkshop\WinAppdriver_automation\WinFormsTests\Data\Application+under+test\DoNotDistrurbMortgageCalculatorFrom1999.exe");
+            appiumOptions.AddAdditionalCapability("app", WinFormsTestsProject.Properties.Settings.Default.ApplicationPath);
+            sessionWinForm = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+        }
+
+        [TestMethod]
+        public void CheckBoxTest()
+        {
+            
+        }
+    }
+}
