ed03978c916c1df9add032961d36794870c58080
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-28 16:30:47 +0200

    wad course

--- WinAppdriver_automation/WinAppDriverForExcelTests/ExcelTests.cs
+++ WinAppdriver_automation/WinAppDriverForExcelTests/ExcelTests.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
 
 namespace WinAppDriverForExcelTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ExcelTests
     {
+        static WindowsDriver<WindowsElement> session;
+
         [TestMethod]
-        public void TestMethod1()
+        public void OpenExcelWithoutSplashScreen()
         {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", "Excel");
+            appiumOptions.AddAdditionalCapability("appArguments", "/e");
+            session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+        }
+
+        [TestMethod]
+        public void OpenExcelFileWithoutSplashScreen()
+        {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", "Excel");
+            appiumOptions.AddAdditionalCapability("appArguments", @"/e c:\Work2\Projects\GIT\EducationWorkshop\WinAppdriver_automation\WinAppDriverForExcelTests\Data\TaskReport.csv");
+            session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
         }
     }
 }
